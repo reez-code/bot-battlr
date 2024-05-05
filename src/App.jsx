73aaa0f68da6e01574d7b3c52f8347883d0ae035
Phil <phil.github@gmail.com>
2024-05-05 11:42:05 +0300

    bot card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import YourBotArmy from "./components/YourBotArmy";
+import BotCollection from "./components/BotCollection";
 
 function App() {
-  useEffect();
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/bots")
+      .then((res) => res.json())
+      .then((data) => setBots(data));
+  });
+
   return (
     <>
       <YourBotArmy />
+      <BotCollection botsData={bots} />
     </>
   );
 }
--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function BotCollection() {
+function BotCollection({ botsData }) {
   return <div></div>;
 }
 
